4c9aa54436f250db62c282b283035ff30cf91376
sujal <sujal.barsaiyan1234@gmail.com>
2024-01-03 15:36:40 +0530

    test for happy and sad mood in constructor

--- src/test/java/TestMoodAnalyzer.java
+++ src/test/java/TestMoodAnalyzer.java
@@ -8,30 +8,25 @@ import com.example.MoodAnalyzer;
 public class TestMoodAnalyzer {
     public MoodAnalyzer moodAnalyzer;
 
-    @BeforeEach
-    public void setUp() {
-        moodAnalyzer = new MoodAnalyzer();
-    }
+    // @BeforeEach
+    // public void setUp() {
+    // moodAnalyzer = new MoodAnalyzer();
+    // }
 
     @Test
     public void testAnalazyMoodSad() {
-        String message = "I am in Sad mood";
-        String result = moodAnalyzer.analyzeMood(message);
+        moodAnalyzer = new MoodAnalyzer("I am in Sad mood");
+        String result = moodAnalyzer.analyzeMood();
 
         assertEquals("SAD", result);
     }
 
     @Test
     public void testAnalazyMoodHappy() {
-        String message = "I am in Any mood";
-        String result = moodAnalyzer.analyzeMood(message);
-
-        assertEquals("HAPPY", result);
-    }
-
-    @Test
-    public void testDefaultMood() {
+        moodAnalyzer = new MoodAnalyzer("I am in Any mood");
         String result = moodAnalyzer.analyzeMood();
+
         assertEquals("HAPPY", result);
     }
+
 }
